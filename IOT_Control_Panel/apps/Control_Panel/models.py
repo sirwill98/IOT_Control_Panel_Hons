636fd435b6ae344b6e0cba630bb6ac8f0b20a04a
Billy <billyboy2410@hotmail.co.uk>
2020-02-05 17:26:39 +0000

    Models Created.

--- IOT_Control_Panel/apps/Control_Panel/models.py
+++ IOT_Control_Panel/apps/Control_Panel/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Map(models.Model):
+    date = models.DateTimeField()
+
+
+class Node(models.Model):
+    Type = models.CharField(max_length=64)
+    Sensor = models.CharField(max_length=64)
+    Status = models.BooleanField()
+    map = models.ForeignKey(Map, on_delete=models.CASCADE)
+
+
+class Reading(models.Model):
+    value = models.CharField(max_length=32)
+    node = models.ForeignKey(Node, on_delete=models.CASCADE)
+    date = models.DateTimeField()
